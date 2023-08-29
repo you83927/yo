58c73c487a6b1e6b716dd2329f7eddd913e71e12
User <you83927@gmail.com>
2023-08-29 17:41:06 +0800

    08/29
    1

--- src/main/java/com/ispan/demo/model/Article.java
+++ src/main/java/com/ispan/demo/model/Article.java
@@ -10,9 +10,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name="article")
+@Data
 public class Article {
 
 	
